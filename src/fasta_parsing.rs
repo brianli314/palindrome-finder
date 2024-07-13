a8828c70c67622abe8dce8ec2965096a5efb550f
AAce3 <107497659+AAce3@users.noreply.github.com>
2024-07-13 19:57:03 -0400

    Update fasta_parsing.rs
    Added TODOs

--- src/fasta_parsing.rs
+++ src/fasta_parsing.rs
@@ -5,6 +5,9 @@ use util::PalindromeData;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
+// TODO: Try and make this function more generic. You can do this in two ways:
+// 1. Have this output an owned string which incurs some overhead but not much
+// 2. Have this function take in a closure as an input. Read https://doc.rust-lang.org/book/ch13-01-closures.html
 pub fn parse_fasta(name: &str, output: &mut Vec<PalindromeData>) {
     let file = match File::open(name) {
         Ok(file) => file,
@@ -14,9 +17,10 @@ pub fn parse_fasta(name: &str, output: &mut Vec<PalindromeData>) {
 
     let mut seq = String::new();
     let mut palins = Vec::new();
-    
+
     for line in reader.lines(){
-        match line{
+        match line {
+            // rename sequence to "line" make it less confusing.
             Ok(sequence) => {
                 if sequence.starts_with(">"){
                     if !seq.is_empty(){
@@ -27,7 +31,7 @@ pub fn parse_fasta(name: &str, output: &mut Vec<PalindromeData>) {
                     seq += &sequence;
                 }
             }
-            Err(error) => panic!("Problem opening the file: {error:?}")
+            Err(error) => panic!("Problem opening the file: {error:?}") // this is the wrong error message. Read the docs for reader.lines()
         }
     }
     run_search(&seq, &mut palins, output)
